ad9403861ca18bf5e4d85cf708ab3500249e32b1
unknown <>
2017-04-24 19:36:04 +0800

    reg_add revised

--- reg_add.php
+++ reg_add.php
@@ -1,6 +1,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <link href="sceen.css" rel="stylesheet" type="text/css" />
-<?php 
+<?php
 require_once ("func.php");
 $error = "";
 $pass = "";
@@ -12,7 +12,7 @@ if( isset($_POST['reg_submit']) ){
 	$role = trim($_POST['role']);
 	$name = trim($_POST['name']);
 	$matchuser = trim($_POST['matchuser']);
-	
+
 	/* validation */
 	if( $userid =="" ){
 		$error .= "帳號不能留空<br/>";
@@ -23,37 +23,37 @@ if( isset($_POST['reg_submit']) ){
 	}elseif( !startsWith($userid, '09') ){
 		$error .= "這不是09開頭的電話號碼喔<br/>";
 	}
-	
+
 	if( $name =="" ){
 		$error .= "名字不能留空<br/>";
 	}
-	
+
 	if( $password =="" ){
 		$error .= "密碼不能留空<br/>";
 	}elseif(strlen($password) <8){
-		$error .= "密碼長度至少為8<br/>";	
+		$error .= "密碼長度至少為8<br/>";
 	}elseif($password != $password1){
-		$error .= "兩次密碼不一樣<br/>";	
+		$error .= "兩次密碼不一樣<br/>";
 	}
 
 
 	if( $role !="parent" && $role != "child" ){
 		$error .= "身份不能留空<br/>";
 	}
-	
+
 		if( $matchuser =="" ){
 		$error .= "配對者不能留空<br/>";
 	}elseif( !db_record_exists('users','userid',$matchuser) && $matchuser !="00" ){
 		$error .= "沒有該配對者<br/>";
 	}
-	
+
 		if( empty($_FILES['file']['name']) ){
 		$profilepic = "Profilepic/default_icon.png";
 	}else{
 		/* insert into db if no error */
 		$target_path = "Profilepic/";
 
-		$target_path = $target_path . basename($_FILES['file']['name']); 
+		$target_path = $target_path . basename($_FILES['file']['name']);
 //		echo  basename($_FILES['file']['tmp_name']);
 		if( move_uploaded_file($_FILES['file']['tmp_name'], $target_path) ) {
 		//    $pass = "file uploded";
@@ -62,11 +62,11 @@ if( isset($_POST['reg_submit']) ){
 		    $error .= "file move fail";
 		}
 	}
-	
+
 /*		 insert into db if no error */
 	if($error ==""){
 		$dbh = my_pdo();
-		$sth = $dbh->prepare("INSERT INTO member(userid,password,name,role,matchuser,profilepic) 	
+		$sth = $dbh->prepare("INSERT INTO member(userid,password,name,role,matchuser,profilepic)
 										 VALUES(:userid,:password,:name,:role,:matchuser,:profilepic) ");
 		$sth->bindParam(":userid", $userid );
 		$sth->bindParam(":password", $password );
@@ -77,7 +77,7 @@ if( isset($_POST['reg_submit']) ){
 		$rtn = $sth->execute();
 		if($rtn){
 			$pass = "註冊成功";
-			unset($_POST);	
+			unset($_POST);
 		}else{
 			$error = "DB error";
 			var_dump($sth->errorInfo());
@@ -100,19 +100,18 @@ if( isset($_POST['reg_submit']) ){
   <div class="content">
 
     <div class="words">
-      <div style="padding:3px;"><a href="index.php">主頁</a> > 註冊</div>
     </div>
-    <div class="passage" style=" width:910px; float:left; height:700px" >
-    <span class="title">註冊</span>
-    <div class="news_edit"> 
+    <div class="passage">
+    <h1 class="title">註冊</h1>
+    <div class="news_edit">
     <?php if($pass){ ?>
-     	<div class="pass"><?php echo $pass ?></div>	 
+     	<div class="pass"><?php echo $pass ?></div>
     <?php } ?>
-    
+
 	<?php if($error){ ?>
     	<div class="error"><?php echo $error ?></div>
     <?php } ?>
-    
+
 	<form enctype="multipart/form-data" action="reg_add.php" method="post">
 		<div class="form_row">
 			<label for="userid">帳號：(手機號碼)</label>
@@ -130,25 +129,27 @@ if( isset($_POST['reg_submit']) ){
 			<label for="name">名字：</label>
 			<input type="text" id="name" name="name" value="<?php P('name'); ?>"/>
 		</div>
-        
+
 		<div class="form_row">
 			<label for="matchuser">配對者帳號：(對方還沒有帳號請先輸入00)</label>
 			<input type="text" id="matchuser" name="matchuser" value="<?php P('matchuser'); ?>"/>
 		</div>
-        
+
 		<div class="form_row">
 			<label for="role">身份：</label>
-            <input type="radio" name="role" id="role" value="parent" 
-			<?php  if( isset($_POST['role']) && $_POST['role'] == "parent") echo "checked";?>>父母
+            <input type="radio" name="role" id="role" value="parent"
+						<?php  if( isset($_POST['role']) && $_POST['role'] == "parent") echo "checked";?>>
+						<h3>父母</h3>
             <input type="radio" name="role" id="role" value="child"
-            <?php  if( isset($_POST['role']) && $_POST['role'] == "child") echo "checked";?>>小孩
+            <?php  if( isset($_POST['role']) && $_POST['role'] == "child") echo "checked";?>>
+						<h3>小孩</h3>
 		</div>
-        
+
         <div class="form_row">
 			<label for="profilepic">頭貼：</label>
             <input type="file" accept="image/*" capture="camera" id="file" name="file"/>
          </div>
-         
+
 		<input type="submit" name="reg_submit" value="Submit"/>
 	</form>
   </div>
