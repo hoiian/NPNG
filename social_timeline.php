e11c95d3baccc443e4cb773f1eabc836ce1e45ac
David <jesus255221@gmail.com>
2017-06-14 16:20:31 +0800

    [UPDATE]Social_timeline.css v0.1

--- social_timeline.php
+++ social_timeline.php
@@ -1,5 +1,5 @@
 <html>
-<?php 
+<?php
 require_once("func.php");
 $grp = $_GET['group'];
 $dbh = my_pdo();
@@ -27,28 +27,33 @@ $sth = $dbh->query("SELECT * FROM  `task` WHERE  `group` LIKE  '$grp' AND `statu
             <a href="profile.php" class="setting"></a>
             <a href="social.php" class="home"></a>
         </div>
-        
+
       <!--  <ul>
             <li><a href="bank.php">存款</a></li>
             <li><a href="history.php">完成任務</a></li>
             <li style="border-bottom:#FFFF8C 3px solid">社群</li>
         </ul>-->
 	</div>
-    
+
 <div class="social">
 	<div class="timeline">
         <ul>
             <?php do{ $row = $sth->fetch(); if($row){?>
             <li>
-                    <img src="<?php echo $row['img'];?>" alt="taskphoto" width="100%" height="auto"/><br/>
+                  <img src="<?php echo $row['img'];?>" alt="taskphoto" width="100%" height="50%"/><br/>
+									<div class="whiteBar">
+										<img width="80px" height="80px" alt"profilepic">
+										<img width="80px" height="80px" alt"profilepic">
+                    <br/>
                     家長帳號:<span class="parent"><?php echo $row['parent'] ?></span><br/>
                     小孩帳號:<span class="child"><?php echo $row['child'] ?></span><br/>
                     任務名稱:<span class="child"><?php echo $row['title'] ?></span><br/>
+									</div>
             </li>
-            <?php } } while($row) ?>   
-          
+            <?php } } while($row) ?>
+
           </ul>
-     
+
       </div>
 
 </div>
